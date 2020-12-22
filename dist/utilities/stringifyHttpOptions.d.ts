d57c90e10eb9baca0ec2503ac294b423f39d7236
Stef <stefvanwijchen@gmail.com>
2020-12-22 17:58:58 +0100

    rm  dist

--- dist/utilities/stringifyHttpOptions.d.ts
+++
@@ -1,7 +0,0 @@
-import { IHttpOptions } from 'nest-utilities-client';
-/**
- * Turns an HTTP options object into a string. Generally useful when comparing.
- * @param options http options
- * @param ids model ids
- */
-export declare function stringifyHttpOptions(options: IHttpOptions<any>, ids?: string[]): string;
